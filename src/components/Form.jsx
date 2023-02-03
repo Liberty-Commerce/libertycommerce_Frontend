3378a498ef1b960e745fcef1bab32f8f02c07aed
andres felipe <andresnarvaez304@gmail.com>
2023-02-02 20:53:40 -0500

    my primer commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+function Form() {
+
+    const [user, setUser] = useState({});
+
+    const getDataInput = (e) =>{
+        const {name , value} = e.target;
+        setUser({[name]:value});
+    }
+
+    const handleForm = (e) => {
+        e.preventDefault;
+        const userName = user.textName;
+        const userPassword = user.password;
+
+        console.log(userName, userPassword);
+    }
+
+
+    return (
+        <div className='p-5'>
+            <h2>Formulario</h2>
+            <form action="" onSubmit={handleForm}>
+                <input type="text" placeholder='Nombre' name='textName' className='mr-3' value = {user.textName} onChange={getDataInput}/> 
+                <input type="password" placeholder='Contraseña' name='password' className='mr-3' value={user.password} onChange={getDataInput}/>
+                <input type="submit" value="Enviar" className='text-white' />
+            </form>
+        </div>
+    )
+}
+
+export default Form
+
