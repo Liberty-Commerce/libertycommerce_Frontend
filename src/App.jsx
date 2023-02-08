81f7e4f4f12d1908a3470297ffd5cde1caebbf3d
Andres Felipe <camachorodriguezandresfelipe@gmail.com>
2023-02-07 22:30:28 -0500

    ordenado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-//Pagina no encontrada:
+//Pagina no encontrada si el usuario no encutra lo que no hay en el sitio web:
 import Notfound from './pagenotfound/Notfound';
 
 //subpaginas:
+//1
+import Registro from "./pages/Registro";
+//2
+import Login from "./pages/Login";
+//3
 import Home from "./pages/Home";
 
 //comentario:develop-barera
@@ -17,8 +22,10 @@ function App() {
         <Routes>
           {/* <Route path="*" element={<Notfound/>}/> */}
           <Route path="*" element={<Notfound/>}/>
+          {/*Registro*/}
+          <Route path="/" element={<Registro />} />
           {/*Login*/}
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           {/*Pagina principal*/}
           <Route path="/home" element={<Home />} />
         </Routes>
--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Login() {
+function Registro() {
     return (
         <>
         <div>
-            <h1>Login</h1>
+            <h1>Registro</h1>
         </div>
         </>
     );
   }
-  export default Login;
+  export default Registro;
