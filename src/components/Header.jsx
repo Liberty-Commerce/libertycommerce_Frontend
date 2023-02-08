1029c76bc3087f1c246cd521fddf94ab22794e55
Andres Felipe <camachorodriguezandresfelipe@gmail.com>
2023-02-08 17:22:51 -0500

    style mejorado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,11 +27,11 @@ export const Header = ({
 	};
 
 	return (
-		<div className="bg-[#333] m-0 m-auto max-w-screen-xl">
-		<header className="flex-auto flex justify-between py-30 px-0 px-40 px-0">
+		<>
+		<header className='flex  justify-between p-8 p-0 p-10 p-0'>
 			<h1>Tienda</h1>
 
-			<div className="relative">
+			<div className='relative'>
 				<div
 					className='container-cart-icon'
 					onClick={() => setActive(!active)}
@@ -42,7 +42,7 @@ export const Header = ({
 						viewBox='0 0 24 24'
 						strokeWidth='1.5'
 						stroke='currentColor'
-						className='icon-cart'
+						className='w-10 h-10 stroke-black hover:cursor-pointer'
 					>
 						<path
 							strokeLinecap='round'
@@ -50,13 +50,13 @@ export const Header = ({
 							d='M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
 						/>
 					</svg>
-					<div className='absolute top-2/4 right-0 bg-black bg-white w-6 h-6 flex justify-center items-center rounded-3xl'>
-						<span className='text-xs'>{countProducts}</span>
+					<div className='absolute top-2/4 right-0 bg-black color:#fff; w-6 h-6 flex justify-center items-center rounded-full'>
+						<span id='text-xs'>{countProducts}</span>
 					</div>
 				</div>
 
-				<div 
-					className={`absolute top-12 right-0 bg-white w-96 z-40 shadow-gray-100 rounded-xl ${
+				<div
+					className={`container-cart-products ${
 						active ? '' : 'hidden-cart'
 					}`}
 				>
@@ -64,15 +64,15 @@ export const Header = ({
 						<>
 							<div className='row-product'>
 								{allProducts.map(product => (
-									<div className='flex items-center justify-between p-8 border-y-2 border-style: solid; bg-black' key={product.id}>
-										<div className='flex justify-between flex-initial'>
-											<span className='font-normal text-xl'>
+									<div className='cart-product' key={product.id}>
+										<div className='info-cart-product'>
+											<span className='cantidad-producto-carrito'>
 												{product.quantity}
 											</span>
-											<p className='text-xl'>
+											<p className='titulo-producto-carrito'>
 												{product.nameProduct}
 											</p>
-											<span className='font-bold text-xl ml-2.5'>
+											<span className='precio-producto-carrito'>
 												${product.price}
 											</span>
 										</div>
@@ -95,21 +95,21 @@ export const Header = ({
 								))}
 							</div>
 
-							<div className='flex justify-center items-center p-5 gap-5'>
-								<h3 className='text-xl font-bold'>Total:</h3>
-								<span className='text-xl font-black'>${total}</span>
+							<div className='cart-total'>
+								<h3>Total:</h3>
+								<span className='total-pagar'>${total}</span>
 							</div>
 
-							<button className='border-none bg-black #f8fafc pl-3.5 block w-full mt-2.5 my-2.5 rounded-b-xl cursor-pointer text-xs ease-in duration-300 font-sans' onClick={onCleanCart}>
+							<button className='btn-clear-all' onClick={onCleanCart}>
 								Vaciar Carrito
 							</button>
 						</>
 					) : (
-						<p className='p-5 text-center text-black'>El carrito está vacío</p>
+						<p className='cart-empty'>El carrito está vacío</p>
 					)}
 				</div>
 			</div>
 		</header>
-		</div>
+		</>
 	);
 };
